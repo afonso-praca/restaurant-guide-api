83a62a8808ed7249ae369392ceca2f0b13f1a1bf
afonso.praca <afonsoinfo@gmail.com>
2013-12-30 22:02:09 -0200

    adds route for updates a restaurant adds route for delete a restaurants adds route for a detail of a restaurant

--- controllers/restaurant-controller.js
+++ controllers/restaurant-controller.js
@@ -5,7 +5,7 @@ var Restaurant = mongoose.model("Restaurant");
 // GET LIST OF RESTAURANTS
 //
 exports.getAllRestaurants = function(req, res){
-	return Restaurant.find(function (err, restaurants) {
+	return Restaurant.find(function (err, restaurants){
 		if (!err) {
 			return res.jsonp(restaurants);
 		} else {
@@ -15,17 +15,54 @@ exports.getAllRestaurants = function(req, res){
 };
 
 //
+// GET A SPECIFIC RESTAURANT BY ID
+//
+exports.getRestaurantById = function(req, res){
+	return res.status(501).send("not implemented");
+};
+
+//
 // CREATES A RESTAURANT
 //
-exports.newRestaurant = function (req, res) {
+exports.newRestaurant = function (req, res){
 	var restaurant = new Restaurant(req.body);
 	restaurant.save(function (err) {
 		if (!err) {
 			console.log("created");
-			return res.jsonp(restaurant);
+			return res.status(201).jsonp(restaurant);
 		} else {
 			console.log(err);
 			return res.send("error");
+		}
+	});
+};
+
+//
+// UPDATES A RESTAURANT
+//
+exports.updateRestaurant = function(req, res){
+	return res.status(501).send("not implemented");
+};
+
+//
+// DELETE A RESTAURANT
+//
+exports.deleteRestaurant = function (req, res){
+	console.log("delete");
+	return Restaurant.findById(req.params.id, function (err, restaurant) {
+		if (restaurant !== null) {
+			return restaurant.remove(function (err) {
+				if (!err) {
+					console.log("removed");
+					return res.send("removed");
+				} else {
+					console.log(err);
+					return res.send("error on remove");
+				}
+			});
+		} else {
+			console.log(err);
+			return res.send("don't fucking have this restaurant");
 		}
 	});
 };
--- routes.js
+++ routes.js
@@ -9,4 +9,7 @@ module.exports = function(app, passport){
 	 */
 	app.get('/api/pvt/restaurants', restaurants.getAllRestaurants);
 	app.post('/api/pvt/restaurants', restaurants.newRestaurant);
+	app.get('/api/pvt/restaurants/:id', restaurants.getRestaurantById);
+	app.put('/api/pvt/restaurants/:id', restaurants.updateRestaurant);
+	app.delete('/api/pvt/restaurants/:id', restaurants.deleteRestaurant);
 };
