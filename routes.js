9b45e56e2e90c6d3d9c84bba946ae8a11523d475
afonso.praca <afonsoinfo@gmail.com>
2014-01-28 12:22:27 -0200

    add a domain on routes to catch errors

--- routes.js
+++ routes.js
@@ -3,13 +3,21 @@ module.exports = function(app, passport){
 	 * Controllers.
 	 */
 	var restaurants = require('./controllers/restaurant-controller');
+	var domain = require('domain');
+	var d = domain.create();
+
+	d.on('error', function(err) {
+		console.error(err);
+	});
 
 	/**
 	 * Restaurants routes.
 	 */
-	app.get('/api/pvt/restaurants', restaurants.getAllRestaurants);
-	app.post('/api/pvt/restaurants', restaurants.newRestaurant);
-	app.get('/api/pvt/restaurants/:id', restaurants.getRestaurantById);
-	app.put('/api/pvt/restaurants/:id', restaurants.updateRestaurant);
-	app.delete('/api/pvt/restaurants/:id', restaurants.deleteRestaurant);
+	d.run(function() {
+		app.get('/api/pvt/restaurants', restaurants.getAllRestaurants);
+		app.post('/api/pvt/restaurants', restaurants.newRestaurant);
+		app.get('/api/pvt/restaurants/:id', restaurants.getRestaurantById);
+		app.put('/api/pvt/restaurants/:id', restaurants.updateRestaurant);
+		app.delete('/api/pvt/restaurants/:id', restaurants.deleteRestaurant);
+	});
 };
