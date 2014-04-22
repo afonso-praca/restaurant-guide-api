e4ae63c970a80505b0e098c73bee50e62b4d897c
afonso.praca <afonsoinfo@gmail.com>
2014-04-22 00:57:45 -0300

    get user email from facebook

--- config/passport.js
+++ config/passport.js
@@ -18,7 +18,8 @@ module.exports = function (passport, facebookAppId, facebookAppSecret) {
 		clientID: facebookAppId,
 		clientSecret: facebookAppSecret,
 		callbackURL: 'http://api.penedorj.com.br/auth/facebook/callback',
-		profileURL: 'https://graph.facebook.com/me?fields=email,location,first_name,last_name,middle_name,name,link,username,work,education,gender,timezone,locale,verified,picture,about,address,age_range,bio,birthday,cover,currency,devices,email,favorite_athletes,id,hometown,favorite_teams,inspirational_people,install_type,installed,interested_in,languages,meeting_for,name_format,political,quotes,relationship_status,religion,significant_other,sports,updated_time,website'
+		profileFields: ['id', 'displayName', 'name', 'photos', 'emails'],
+		profileURL: 'https://graph.facebook.com/me?fields=location,first_name,last_name,middle_name,name,link,username,work,education,gender,timezone,locale,verified,picture,about,address,age_range,bio,birthday,cover,currency,devices,email,favorite_athletes,id,hometown,favorite_teams,inspirational_people,install_type,installed,interested_in,languages,meeting_for,name_format,political,quotes,relationship_status,religion,significant_other,sports,updated_time,website'
 	}, function(accessToken, refreshToken, profile, done) {
 		console.log(profile);
 		User.findOne({ 'facebook.id': profile.id }, function (err, user) {
