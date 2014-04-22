dbdaed7b6633a3c55dde44ef3d234994c438ccdf
afonso.praca <afonsoinfo@gmail.com>
2014-04-22 18:15:06 -0300

    adjusts fb callback

--- config/passport.js
+++ config/passport.js
@@ -17,7 +17,7 @@ module.exports = function (passport, facebookAppId, facebookAppSecret) {
 	passport.use(new FacebookStrategy({
 		clientID: facebookAppId,
 		clientSecret: facebookAppSecret,
-		callbackURL: 'http://api.penedorj.com.br/auth/facebook/callback',
+		callbackURL: 'http://penedorj.com.br/api/auth/facebook/callback',
 		profileFields: ['id', 'displayName', 'name', 'photos', 'emails'],
 		profileURL: 'https://graph.facebook.com/me?fields=location,first_name,last_name,middle_name,name,link,username,work,education,gender,timezone,locale,verified,picture,about,address,age_range,bio,birthday,cover,currency,devices,email,favorite_athletes,id,hometown,favorite_teams,inspirational_people,install_type,installed,interested_in,languages,meeting_for,name_format,political,quotes,relationship_status,religion,significant_other,sports,updated_time,website'
 	}, function(accessToken, refreshToken, profile, done) {
